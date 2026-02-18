7ae8141f9667d046dc72ae3dda9cfdb7ddacf368
GULLAPALLI DURGA SANKAR RAO 21BCE9132 <durgasankar.21bce9132@vitapalum.ac.in>
2026-02-18 17:22:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5881978474-368c1",
+  "appId": "1:359559422336:web:c015db2558ccad615b2165",
+  "apiKey": "AIzaSyDqKZ9_KEfRrxOglGEI2p_2zll5V8TUeFc",
+  "authDomain": "studio-5881978474-368c1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "359559422336"
+};
